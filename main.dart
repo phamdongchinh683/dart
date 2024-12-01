d9ec2a46baf02e3ba1a577863d887be431cbfaf2
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-02 00:12:01 +0700

    variables 2-12-2024

--- main.dart
+++ main.dart
@@ -1,3 +1,8 @@
-void main() {
-  print('Hello, World!');
-}
+var name = 'Voyager I'; // string
+var year = 1977; // int
+var antennaDiameter = 3.7; // double
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune']; // array
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+}; // object
