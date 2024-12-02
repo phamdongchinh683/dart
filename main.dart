e57c67690f014bccb589cf5b004d3d5216602547
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-02 22:40:11 +0700

    null safety 2-12-2024

--- main.dart
+++ main.dart
@@ -1,8 +1,6 @@
-var name = 'Voyager I'; // string
-var year = 1977; // int
-var antennaDiameter = 3.7; // double
-var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune']; // array
-var image = {
-  'tags': ['saturn'],
-  'url': '//path/to/saturn.jpg'
-}; // object
+String? name; // Nullable type. Can be `null` or string.
+String name   // Non-nullable type. Cannot be `null` but can be string.
+void main() {
+  print(name);
+}
+
