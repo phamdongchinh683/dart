387f2debb0cb1687f8fd558c9c47a605ff66fd78
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-05 12:45:53 +0700

    collections map 5-12-2024

--- main.dart
+++ main.dart
@@ -1,23 +1,30 @@
 void main() {
-  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-  var names = <String>{};
-// Set<String> names = {}; // This works, too.
-  // var names = {}; // Creates a map, not a set.
-
-  var elements = <String>{};
-  elements.add('fluorine');
-  elements.add('fluorine2');
-
-  elements.addAll(halogens);
-  print(elements);
-  // {fluorine, fluorine2, chlorine, bromine, iodine, astatine}
-
-  final constantSet = const {
-    'fluorine',
-    'chlorine',
-    'bromine',
-    'iodine',
-    'astatine',
+  var gifts = {
+    // Key:    Value
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 'golden rings'
   };
-// constantSet.add('helium'); // This line will cause an error.
+
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+  };
+
+  //You can create the same objects using a Map constructor:
+  var gift2 = Map<String, String>();
+  gift2['first'] = 'partridge';
+  gift2['second'] = 'turtledoves';
+  gift2['fifth'] = 'golden rings';
+
+  var nobleGase2 = Map<int, String>();
+  nobleGase2[2] = 'helium';
+  nobleGase2[10] = 'neon';
+  nobleGase2[18] = 'argon';
+
+  var gifts2 = {'first': 'partridge'};
+  gifts2['fourth'] = 'calling birds'; // Add a key-value pair
+  print(gifts2); // {first: partridge, fourth: calling birds}
+  print(nobleGase2); // {first: partridge, fourth: calling birds}
 }
