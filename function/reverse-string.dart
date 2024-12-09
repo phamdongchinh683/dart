bda40a3687f786770177e2a1c8a123fe25cfabd0
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-10 00:36:21 +0700

    create list of names print all it 10-12-2024

--- function/reverse-string.dart
+++
@@ -1,9 +0,0 @@
-String reverseString(String text) {
-  //split the string convert array
-  return text.split('').reversed.join('');
-}
-
-void main() {
-  const example = "Music for the win";
-  print(reverseString(example));
-}
---
+++ main.dart
@@ -0,0 +1,4 @@
+void main(){
+  const List<String> names = ["Chinh","Chinh","Truong","Hong"];
+  print(names);
+}
