1aac2ba7360b531c452bd88d6d69e56efcf9b502
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-10 22:01:30 +0700

    object in dart 10-12-2024

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'OOP/class-dart.dart';
+
+void main() {
+  Animal animal = Animal();
+
+  animal.name = "Monkey";
+  animal.numberOfLegs = 144;
+  animal.lifeSpan = 20;
+
+  animal.display();
+//  Animal name: Monkey.
+// Number of Legs: 144.
+// Life Span: 20.
+
+}
