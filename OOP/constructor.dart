b55533830d9b54c9745020af96ea57ba02ec90c2
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-10 22:20:28 +0700

    namedConstructor in dart 10-12-2024

--- OOP/constructor.dart
+++ OOP/constructor.dart
@@ -1,20 +1,30 @@
-class Student {
+class Animal {
   String? name;
   int? age;
-  int? rollNumber;
 
-  // Constructor
-  Student({String? name, int? age, int? rollNumber}) {
+  // Default Constructor
+  Animal() {
+    print("This is a default constructor");
+  }
+
+  // Named Constructor
+  Animal.namedConstructor(String name, int age) {
     this.name = name;
     this.age = age;
-    this.rollNumber = rollNumber;
+  }
+
+  // Named Constructor
+  Animal.namedConstructor2(String name) {
+    this.name = name;
   }
 }
 
 void main() {
-  // Here student is object of class Student.
-  Student student = Student(name: "John", age: 20, rollNumber: 1);
-  print("Name: ${student.name}");
-  print("Age: ${student.age}");
-  print("Roll Number: ${student.rollNumber}");
+  // Here animal is object of class Animal.
+  Animal animal = Animal.namedConstructor("Dog", 5);
+  print("Name: ${animal.name}");
+  print("Age: ${animal.age}");
+
+  Animal animal2 = Animal.namedConstructor2("Cat");
+  print("Name: ${animal2.name}");
 }
--- main.dart
+++ main.dart
@@ -1,9 +1,11 @@
 import 'OOP/constructor.dart';
 
 void main() {
-  // Here student is object of class Student.
-  Student student = Student(name: "John", age: 20, rollNumber: 1);
-  print("Name: ${student.name}");
-  print("Age: ${student.age}");
-  print("Roll Number: ${student.rollNumber}");
+  // Here animal is object of class Animal.
+  Animal animal = Animal.namedConstructor("Dog", 5);
+  print("Name: ${animal.name}");
+  print("Age: ${animal.age}");
+
+  Animal animal2 = Animal.namedConstructor2("Cat");
+  print("Name: ${animal2.name}");
 }
