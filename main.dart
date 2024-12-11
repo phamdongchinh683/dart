278c5395f3cddc7301942b7ff5ca876ef83cafc5
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-11 22:54:25 +0700

    private properties in dart 11-10-2024

--- main.dart
+++ main.dart
@@ -1,13 +1,7 @@
 import 'employee.dart';
 
 void main() {
-  // Create an object of Employee class
-  Employee emp = new Employee();
-  // setting values to the object using setter
-  emp.setId(1);
-  emp.setName("John");
-
-  // Retrieve the values of the object using getter
-  print("Id: ${emp.getId()}");
-  print("Name: ${emp.getName()}");
+  var employee = Employee();
+  employee.setName("Jack");
+  print(employee.getName());
 }
