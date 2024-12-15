a7dbbd6d5842e79b7a5828759ceb110495dca1c2
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-15 23:24:37 +0700

    static in dart 15-12-2024

--- main.dart
+++ main.dart
@@ -1,9 +1,22 @@
-import 'Person.dart';
+class Employee {
+  // Static variable
+  static int count = 0;
+  // Constructor
+  Employee() {
+    count++;
+  }
+  // Method to display the value of count
+  void totalEmployee() {
+    print("Total Employee: $count");
+  }
+}
 
 void main() {
-  Person p1 = Person("John", "Doe", Gender.Male);
-  p1.display();
-
-  Person p2 = Person("Menuka", "Sharma", Gender.Female);
-  p2.display();
+  // Creating objects of Employee class
+  Employee e1 = new Employee();
+  e1.totalEmployee();
+  Employee e2 = new Employee();
+  e2.totalEmployee();
+  Employee e3 = new Employee();
+  e3.totalEmployee();
 }
