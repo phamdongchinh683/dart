eb5a16d3bfe8f8d359bcc016944bcb88e5c05608
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-18 23:13:16 +0700

    Write a dart program to create an enum class for gender [male, female, others] and print all values 18-12-2024

--- main.dart
+++ main.dart
@@ -1,16 +1,9 @@
-import 'House.dart';
+import 'gender.dart';
 
 void main() {
-  List<House> houses = [];
-  House house1 = new House("1", "second floor", "20000.");
-  houses.add(house1);
-  House house2 = new House("2", "three floor", "40000.");
-  houses.add(house2);
-
-  House house3 = new House("3", "four floor", "70000.");
-  houses.add(house3);
-
-  for (var house in houses) {
-    house.displayDetails();
+  // Print all enum values
+  print('Gender values:');
+  for (var gender in Gender.values) {
+    print(gender);
   }
 }
