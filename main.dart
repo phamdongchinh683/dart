6f62bd002b6fdc4ea7a1605e9833d83de0a6ec9a
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-12-18 23:27:11 +0700

    Write a dart program to create a class Camera with private properties [id, brand, color, price]. Create a getter and setter to get and set values. Also, create 3 objects of it and print all details 18-12-2024

--- main.dart
+++ main.dart
@@ -1,7 +1,13 @@
-import 'Cat.dart';
+import 'Camera.dart';
 
 void main() {
-  Cat myCat = Cat(1, 'White', 'Snowball', 'Meow');
+  Camera camera1 = Camera(1, 'Canon', 'Black', 500.00);
+  Camera camera2 = Camera(2, 'Nikon', 'Silver', 600.00);
+  Camera camera3 = Camera(3, 'Sony', 'Red', 700.00);
 
-  myCat.displayDetails();
+  camera1.price = 550.00;
+  List<Camera> cameras = [camera1, camera2, camera3];
+  for (var camera in cameras) {
+    camera.displayDetails();
+  }
 }
